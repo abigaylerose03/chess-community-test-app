fb258f1438d0723af38ae6509ea4feee950f8585
Abigayle Peterson <abigaylerose03@gmail.com>
2024-04-16 15:03:03 -0700

    i

---
+++ chess-community-app/src/ChessCommunity.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ChessCommunity() {
+  return (
+    <div>
+      <h1>Welcome to the Chess Community</h1>
+      <p>This is a place for chess enthusiasts to connect, learn, and play!</p>
+      {/* Add more components like Leaderboard, Chat, Chessboard, etc. */}
+    </div>
+  );
+}
+
+export default ChessCommunity;
